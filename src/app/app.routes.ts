ac96984cdc600e020cd033697db0ba7af1913e76
s3bs4n <SEB.SANTIBANEZG@DUOCUC.CL>
2024-06-21 23:03:08 -0400

    Commit seguro antes de comenzar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { ListaProductosComponent } from './components/lista-productos/lista-productos.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {path: 'lista-productos' , component: ListaProductosComponent},
+    {path: '**' , redirectTo: 'lista-productos'},
+    
+
+
+
+];

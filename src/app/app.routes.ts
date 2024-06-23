a3af8de729c0d7d49a6f772b5e35486321244fc2
s3bs4n <SEB.SANTIBANEZG@DUOCUC.CL>
2024-06-22 22:11:11 -0400

    pagina carrito funcionando

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,14 @@
 import { Routes } from '@angular/router';
+import { CarritoComponent } from './components/carrito/carrito.component';
 import { ListaProductosComponent } from './components/lista-productos/lista-productos.component';
 
 export const routes: Routes = [
 
-    {path: 'lista-productos' , component: ListaProductosComponent},
-    {path: '**' , redirectTo: 'lista-productos'},
+    {path: 'carrito' , component: CarritoComponent},
+    {path: '**' , redirectTo: 'carrito'},
     
+    // {path: 'lista-productos' , component: ListaProductosComponent},
+    // {path: '**' , redirectTo: 'lista-productos'},
 
 
 
